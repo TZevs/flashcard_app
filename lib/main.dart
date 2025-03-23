6f10ff3fb968ebade812ff2b9acf825c5037ad05
Thalia <148969711+TZevs@users.noreply.github.com>
2025-03-23 15:31:30 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyFlashCardApp());
+}
+
+class MyFlashCardApp extends StatelessWidget {
+  const MyFlashCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
