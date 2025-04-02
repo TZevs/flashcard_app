a156ef5d80d5bdb7bcb419d1bee2c60bb385c8df
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-02 16:11:55 +0100

    Setting up the file structure and basic files

---
+++ lib/views/flashcard_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlashcardScreen extends StatelessWidget {
+  const FlashcardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
