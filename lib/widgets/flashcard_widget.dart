5c2dd8bb2c64906358a12c6fa91916b4a5443bcb
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-03 12:24:45 +0100

    stateful for animation logic

---
+++ lib/widgets/flashcard_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FlashcardWidget extends StatefulWidget {
+  const FlashcardWidget({super.key});
+
+  @override
+  State<FlashcardWidget> createState() => _FlashcardWidget();
+}
+
+class _FlashcardWidget extends State<FlashcardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        
+      )
+    );
+  }
+}
