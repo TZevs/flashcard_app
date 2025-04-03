3e3816c2de6dfc095fda19567884347df61df52c
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-03 12:59:17 +0100

    Convert object to map

--- lib/models/flashcard_model.dart
+++ lib/models/flashcard_model.dart
@@ -1,6 +1,17 @@
 class FlashcardModel {
+  final int? id;
+  final int deckId;
   final String cardFront;
   final String cardBack;
 
-  FlashcardModel({ required this.cardFront, required this.cardBack });
+  FlashcardModel({ this.id, required this.deckId, required this.cardFront, required this.cardBack });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'deckId': deckId,
+      'cardFront': cardFront,
+      'cardBack': cardBack,
+    };
+  }
 }
