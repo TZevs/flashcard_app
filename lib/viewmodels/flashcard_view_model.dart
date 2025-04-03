4b6241a4dc39212276eaf0e644d5f84f285195c6
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-03 12:59:51 +0100

    Started

---
+++ lib/viewmodels/flashcard_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flashcard_app/models/flashcard_model.dart';
+import 'package:flutter/material.dart';
+
+class FlashcardViewModel extends ChangeNotifier {
+  final List<FlashcardModel> _flashcards = [];
+}
