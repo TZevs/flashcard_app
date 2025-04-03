3afd60221e4c77d9c3ac5ca316d0ec347589cb34
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-03 16:19:01 +0100

    Added methods for getting decks and flashcards from the db

--- lib/viewmodels/flashcard_view_model.dart
+++ lib/viewmodels/flashcard_view_model.dart
@@ -1,6 +1,26 @@
+import 'package:flashcard_app/models/deck_model.dart';
 import 'package:flashcard_app/models/flashcard_model.dart';
+import 'package:flashcard_app/services/flashcard_db.dart';
 import 'package:flutter/material.dart';
 
 class FlashcardViewModel extends ChangeNotifier {
-  final List<FlashcardModel> _flashcards = [];
+  List<DeckModel> _decks = [];
+  List<FlashcardModel> _flashcards = [];
+
+  // Gets the current deck and flashcard lists
+  List<DeckModel> get decks => _decks;
+  List<FlashcardModel> get cards => _flashcards;
+  // Possible add a loading bool and an error message string
+
+  // Gets the Decks from the DB
+  Future<void> fetchDecks() async {
+    // Add try and catch
+    _decks = await FlashcardDb.getDecks();
+    notifyListeners();
+  }
+
+  Future<void> fetchDeckFlashcards(int deckID) async {
+    _flashcards = await FlashcardDb.getDeckFlashcards(deckID);
+    notifyListeners();
+  }
 }
