aa723e1a664db6c1bb5cd1ec10d4af38ea7805c8
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-07 14:22:24 +0100

    Formatted changed id to String with uuid package

--- lib/models/deck_model.dart
+++ lib/models/deck_model.dart
@@ -1,25 +1,22 @@
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
+
 class DeckModel {
-  final int id;
+  final String id;
   final String title;
   final bool isPublic;
 
-  DeckModel({ required this.id, required this.title, required this.isPublic });
+  DeckModel({required this.id, required this.title, required this.isPublic});
 
   // Converts Deck obejct to map
   Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'title': title,
-      'isPublic': isPublic
-    };
+    return {'id': id, 'title': title, 'isPublic': isPublic};
   }
 
   // Converts map to a Deck object
   factory DeckModel.fromMap(Map<String, dynamic> deck) {
     return DeckModel(
-      id: deck['id'], 
-      title: deck['title'], 
-      isPublic: deck['isPublic']
-    );
+        id: deck['id'], title: deck['title'], isPublic: deck['isPublic']);
   }
 }
--- lib/models/flashcard_model.dart
+++ lib/models/flashcard_model.dart
@@ -1,10 +1,14 @@
 class FlashcardModel {
-  final int? id;
-  final int deckId;
+  final String? id;
+  final String deckId;
   final dynamic cardFront;
   final dynamic cardBack;
 
-  FlashcardModel({ this.id, required this.deckId, required this.cardFront, required this.cardBack });
+  FlashcardModel(
+      {this.id,
+      required this.deckId,
+      required this.cardFront,
+      required this.cardBack});
 
   // Converts Flashcard object to a map for sqflite
   Map<String, dynamic> toMap() {
@@ -19,10 +23,9 @@ class FlashcardModel {
   // Converts map to a Flashcard object
   factory FlashcardModel.fromMap(Map<String, dynamic> card) {
     return FlashcardModel(
-      id: card['id'],
-      deckId: card['deckId'], 
-      cardFront: card['cardFront'], 
-      cardBack: card['cardBack']
-    );
+        id: card['id'],
+        deckId: card['deckId'],
+        cardFront: card['cardFront'],
+        cardBack: card['cardBack']);
   }
 }
