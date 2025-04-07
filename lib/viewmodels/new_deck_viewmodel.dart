6eabe5371007c7a81dc9167e76d47ddb99b197f0
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-07 15:56:28 +0100

    Changes so that I am not using a stateful widget

--- lib/viewmodels/new_deck_viewmodel.dart
+++ lib/viewmodels/new_deck_viewmodel.dart
@@ -1,28 +1,59 @@
 import 'package:flashcard_app/models/deck_model.dart';
 import 'package:flashcard_app/models/flashcard_model.dart';
+import 'package:flashcard_app/services/flashcard_db.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 var uuid = Uuid();
 
 class NewDeckViewmodel extends ChangeNotifier {
-  List<FlashcardModel> _newFlashcards = [];
-  var theDeckId = uuid.v4();
+  List<FlashcardModel> newFlashcards = [];
+  String deckTitle = "";
+  bool isPublic = false;
+  String theDeckId = uuid.v4();
 
-  void addNewDeck(String deckName, bool public) {
-    DeckModel(id: theDeckId, title: deckName, isPublic: public);
+  void setDeckTitle(String title) {
+    deckTitle = title;
+    notifyListeners();
   }
 
-  void addFlashcardsToDB(List<FlashcardModel> cards) {}
+  void setIsPublic(bool bool) {
+    isPublic = bool;
+    notifyListeners();
+  }
 
   void addFlashcard(dynamic front, dynamic back) {
-    _newFlashcards.add(
+    newFlashcards.add(
         FlashcardModel(deckId: theDeckId, cardFront: front, cardBack: back));
     notifyListeners();
   }
 
   void removeFlashcard(int index) {
-    _newFlashcards.removeAt(index);
+    newFlashcards.removeAt(index);
+    notifyListeners();
+  }
+
+  void editFlashcard(int index, dynamic front, dynamic back) {
+    var toEdit = newFlashcards[index];
+    // Add getters to the flashcard model to change the card front and backs.
+  }
+
+  void addNewDeck() async {
+    DeckModel newDeck =
+        DeckModel(id: theDeckId, title: deckTitle, isPublic: isPublic);
+
+    await FlashcardDb.addDeck(newDeck);
+    addFlashcardsToDB();
+  }
+
+  void addFlashcardsToDB() async {
+    await FlashcardDb.addDeckFlashcards(newFlashcards);
+  }
+
+  void reset() {
+    newFlashcards = [];
+    deckTitle = "";
+    isPublic = false;
     notifyListeners();
   }
 }
