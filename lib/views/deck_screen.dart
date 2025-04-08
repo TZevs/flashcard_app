6f67bc3f81f46b8ed028ef615d73a56e9d43bfdd
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-08 12:59:26 +0100

    Created layout for screen and the list builder for the decks. Added a button to go to the createDeckScreen

--- lib/views/deck_screen.dart
+++ lib/views/deck_screen.dart
@@ -1,19 +1,47 @@
+import 'package:flashcard_app/viewmodels/flashcard_viewmodel.dart';
+import 'package:flashcard_app/views/create_deck_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DeckScreen extends StatelessWidget {
   const DeckScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
+    return Consumer<FlashcardViewModel>(builder: (context, viewModel, child) {
+      return SafeArea(
+          child: Scaffold(
         appBar: AppBar(
           title: Text("Decks"),
         ),
         body: Column(
-          
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: viewModel.currentDecks.length,
+                    itemBuilder: (context, index) {
+                      final deck = viewModel.currentDecks[index];
+                      return ListTile(
+                        title: Text(deck.title),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                                icon: Icon(Icons.edit), onPressed: () {}),
+                            IconButton(
+                                icon: Icon(Icons.delete), onPressed: () {}),
+                          ],
+                        ),
+                        onTap: () {},
+                      );
+                    })),
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (ctx) => CreateDeckScreen()))),
+          ],
         ),
-      )
-    );
+      ));
+    });
   }
 }
