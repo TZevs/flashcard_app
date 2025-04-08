14599c7c04fd437f2380c2cca4656db2795fc8b5
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-08 16:10:48 +0100

    Added the FlipCard package and component

--- lib/views/flashcard_screen.dart
+++ lib/views/flashcard_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flashcard_app/viewmodels/flashcard_viewmodel.dart';
+import 'package:flashcard_app/widgets/flashcard_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_flip_card/flutter_flip_card.dart';
 import 'package:provider/provider.dart';
 
 class FlashcardScreen extends StatelessWidget {
   final String deckID;
-  const FlashcardScreen({Key? key, required this.deckID}) : super(key: key);
+  // const FlashcardScreen({Key? key, required this.deckID}) : super(key: key);
+  const FlashcardScreen({super.key, required this.deckID});
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +22,20 @@ class FlashcardScreen extends StatelessWidget {
         body: Column(
           children: [
             Text(deckTitle),
+            SizedBox(height: 25),
             Center(
-              child: Card(),
+              child: FlipCard(
+                frontWidget: FlashcardWidget(
+                    content: deckFlashcards[cardIndex].cardFront),
+                backWidget: FlashcardWidget(
+                    content: deckFlashcards[cardIndex].cardBack),
+                controller: FlipCardController(),
+                rotateSide: RotateSide.left,
+                onTapFlipping: true,
+                axis: FlipAxis.horizontal,
+              ),
             ),
+            SizedBox(height: 10),
             Text("${cardIndex + 1} / ${viewModel.deckLength}"),
           ],
         ),
