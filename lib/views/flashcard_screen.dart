d5e9261977388f20021f6df96f2faf7b13409b39
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-08 14:27:52 +0100

    Working on Flashcard_screen

--- lib/views/flashcard_screen.dart
+++ lib/views/flashcard_screen.dart
@@ -1,10 +1,31 @@
+import 'package:flashcard_app/viewmodels/flashcard_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FlashcardScreen extends StatelessWidget {
-  const FlashcardScreen({super.key});
+  final String deckID;
+  const FlashcardScreen({Key? key, required this.deckID}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<FlashcardViewModel>(builder: (context, viewModel, child) {
+      final deckTitle = viewModel.getDeckTitle(deckID);
+      viewModel.fetchDeckFlashcards(deckID);
+      final deckFlashcards = viewModel.currentCards;
+      int cardIndex = 0;
+
+      return SafeArea(
+          child: Scaffold(
+        body: Column(
+          children: [
+            Text(deckTitle),
+            Center(
+              child: Card(),
+            ),
+            Text("${cardIndex + 1} / ${viewModel.deckLength}"),
+          ],
+        ),
+      ));
+    });
   }
 }
