2e530fbb52ab479c7433505031adff731c3269a4
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-15 13:30:32 +0100

    global theme

---
+++ lib/widgets/themes/main_themes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+final ThemeData globalTheme = ThemeData(
+  primaryColor: Color(0xFF2D4B48),
+  listTileTheme: ListTileThemeData(
+    tileColor: Color(0xFF5c8966),
+    textColor: Color(0xFFEBE4C2),
+    iconColor: Color(0xFFEBE4C2),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    titleTextStyle: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+    subtitleTextStyle: TextStyle(
+      fontSize: 16,
+    ),
+  ),
+  appBarTheme: AppBarTheme(
+      backgroundColor: Color(0xFF30253e),
+      iconTheme: IconThemeData(color: Color(0xFFEBE4C2)),
+      titleTextStyle: TextStyle(
+          color: Color(0xFFEBE4C2), fontSize: 35, fontWeight: FontWeight.bold)),
+  scaffoldBackgroundColor: Color(0xFF2D4B48),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Color(0xFF7BB497),
+    foregroundColor: Color(0xFF30253e),
+  ),
+  cardTheme: CardTheme(
+    color: Color(0xFF5c8966),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+  ),
+);
