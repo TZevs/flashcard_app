28bbd4c0c0f1cb7d636100a028b4108b66a3b7c0
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-15 13:31:43 +0100

    flashcard widget theme

---
+++ lib/widgets/themes/flashcard_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final ThemeData flashCardTheme = ThemeData(
+  cardTheme: CardTheme(
+    color: const Color(0xFF71bc78),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    clipBehavior: Clip.hardEdge,
+    shadowColor: const Color(0xFFEBE4C2),
+  ),
+);
