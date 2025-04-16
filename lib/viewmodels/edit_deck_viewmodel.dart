eefcc066347a628537446179ebc49ac12bdef7c9
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-16 16:46:18 +0100

    Can now edit decks and cards. Bug resolved.

--- lib/viewmodels/edit_deck_viewmodel.dart
+++ lib/viewmodels/edit_deck_viewmodel.dart
@@ -1,15 +1,56 @@
+import 'package:flashcard_app/models/deck_model.dart';
 import 'package:flashcard_app/models/flashcard_model.dart';
+import 'package:flashcard_app/services/flashcard_db.dart';
 import 'package:flutter/material.dart';
 
 class EditDeckViewmodel extends ChangeNotifier {
   List<FlashcardModel> _flashcards = [];
   List<FlashcardModel> get getFlashcards => _flashcards;
-  String theDeckId = "";
+  List<FlashcardModel> _newFlashcards = [];
+
+  List<FlashcardModel> editedCards = [];
+  List<int> deletedCards = [];
+
+  late DeckModel deckToEdit;
 
   String deckTitle = "";
   bool isPublic = false;
 
-  String publicOrPrivateLabel = "Make Public?";
+  String publicOrPrivateLabel = "";
+
+  Future<void> fetchDeckFlashcards(String deckID) async {
+    await Future.delayed(Duration(seconds: 1));
+    _flashcards = await FlashcardDb.getDeckFlashcards(deckID);
+    notifyListeners();
+  }
+
+  Future<void> updateCards() async {
+    if (deletedCards.isNotEmpty) {
+      await FlashcardDb.deleteFlashcards(deletedCards);
+    } else if (editedCards.isNotEmpty) {
+      for (var card in editedCards) {
+        await FlashcardDb.updateFlashcards(card);
+      }
+    } else if (_newFlashcards.isNotEmpty) {
+      await FlashcardDb.addDeckFlashcards(_newFlashcards);
+    }
+    notifyListeners();
+  }
+
+  Future<void> updateDeck() async {
+    DeckModel newDeck = DeckModel(
+      id: deckToEdit.id,
+      title: deckTitle,
+      isPublic: isPublic,
+      cardCount: _flashcards.length,
+    );
+
+    // print("Edited deck: ${deckTitle}");
+    // print("Edited deck: ${isPublic}");
+    // print("Edited deck: ${_flashcards.length}");
+    await FlashcardDb.updateDeck(newDeck);
+    notifyListeners();
+  }
 
   void setDeckTitle(String title) {
     deckTitle = title;
@@ -27,18 +68,30 @@ class EditDeckViewmodel extends ChangeNotifier {
   }
 
   void addFlashcard(String front, String back) {
-    _flashcards.add(
-        FlashcardModel(deckId: theDeckId, cardFront: front, cardBack: back));
+    FlashcardModel newCard =
+        FlashcardModel(deckId: deckToEdit.id, cardFront: front, cardBack: back);
+    _flashcards.add(newCard);
+    _newFlashcards.add(newCard);
     notifyListeners();
   }
 
   void removeFlashcard(int index) {
-    _flashcards.removeAt(index);
+    if (_flashcards[index].id != null) {
+      deletedCards.add(_flashcards[index].id!);
+      _flashcards.removeAt(index);
+    } else {
+      _flashcards.removeAt(index);
+      _newFlashcards.removeAt(index);
+    }
     notifyListeners();
   }
 
   void editFlashcard(int index, String front, String back) {
-    var toEdit = _flashcards[index];
+    FlashcardModel toEdit;
+    if (_flashcards[index].id != null) {
+      editedCards.add(_flashcards[index]);
+    }
+    toEdit = _flashcards[index];
     toEdit.cardFront = front;
     toEdit.cardBack = back;
     notifyListeners();
--- lib/views/edit_deck_screen.dart
+++ lib/views/edit_deck_screen.dart
@@ -1,10 +1,13 @@
+import 'package:flashcard_app/models/deck_model.dart';
+import 'package:flashcard_app/models/flashcard_model.dart';
 import 'package:flashcard_app/viewmodels/edit_deck_viewmodel.dart';
 import 'package:flashcard_app/widgets/appbar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class EditDeckScreen extends StatefulWidget {
-  const EditDeckScreen({super.key});
+  final DeckModel deck;
+  const EditDeckScreen({super.key, required this.deck});
 
   @override
   State<EditDeckScreen> createState() => _EditDeckScreenState();
@@ -15,6 +18,71 @@ class _EditDeckScreenState extends State<EditDeckScreen> {
   final TextEditingController _cardFrontController = TextEditingController();
   final TextEditingController _cardBackController = TextEditingController();
 
+  void _showEditBox(BuildContext context, int index, FlashcardModel card,
+      EditDeckViewmodel viewModel) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Edit Flashcard"),
+              icon: IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () => Navigator.pop(context)),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: TextField(
+                      controller: _cardFrontController,
+                      decoration: InputDecoration(
+                        labelText: "Card Front",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: TextField(
+                      controller: _cardBackController,
+                      decoration: InputDecoration(
+                        labelText: "Card Back",
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        viewModel.editFlashcard(
+                            index,
+                            _cardFrontController.text,
+                            _cardBackController.text);
+                        Navigator.pop(context);
+                      },
+                      child: Text("Save"))
+                ],
+              ),
+            ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final viewModel = Provider.of<EditDeckViewmodel>(context, listen: false);
+    viewModel.fetchDeckFlashcards(widget.deck.id);
+    viewModel.deckToEdit = widget.deck;
+    viewModel.deckTitle = widget.deck.title;
+    viewModel.isPublic = widget.deck.isPublic;
+    viewModel.publicOrPrivateLabel =
+        viewModel.isPublic ? "Make Private?" : "Make Public?";
+    _titleController.text = viewModel.deckTitle;
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _cardFrontController.dispose();
+    _cardBackController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<EditDeckViewmodel>(builder: (context, viewModel, child) {
@@ -22,6 +90,91 @@ class _EditDeckScreenState extends State<EditDeckScreen> {
           child: Scaffold(
         appBar: AppbarWidget(
           title: "Edit Deck",
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(7.5),
+              child: TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  hintText: "Deck Title",
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: viewModel.setDeckTitle,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(7.5),
+              child: SwitchListTile(
+                  value: viewModel.deckToEdit.isPublic,
+                  title: Text(viewModel.publicOrPrivateLabel),
+                  onChanged: viewModel.setIsPublic),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(7.5),
+              child: TextField(
+                controller: _cardFrontController,
+                decoration: InputDecoration(
+                  labelText: "Card Front",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(7.5),
+              child: TextField(
+                controller: _cardBackController,
+                decoration: InputDecoration(
+                  labelText: "Card Back",
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (_cardFrontController.text.isNotEmpty &&
+                      _cardBackController.text.isNotEmpty) {
+                    viewModel.addFlashcard(
+                        _cardFrontController.text, _cardBackController.text);
+
+                    _cardFrontController.clear();
+                    _cardBackController.clear();
+                  }
+                },
+                child: Text("Add Flashcard")),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: viewModel.getFlashcards.length,
+                    itemBuilder: (context, index) {
+                      final item = viewModel.getFlashcards[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(7.5),
+                        child: ListTile(
+                          title: Text(item.cardFront),
+                          subtitle: Text(item.cardBack),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                  icon: Icon(Icons.edit),
+                                  onPressed: () => _showEditBox(
+                                      context, index, item, viewModel)),
+                              IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () =>
+                                      viewModel.removeFlashcard(index)),
+                            ],
+                          ),
+                        ),
+                      );
+                    })),
+            ElevatedButton(
+                onPressed: () {
+                  viewModel.updateDeck();
+                  viewModel.updateCards();
+                  Navigator.pop(context);
+                },
+                child: Text("Update Deck")),
+          ],
         ),
       ));
     });
