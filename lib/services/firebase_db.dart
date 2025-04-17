e160718f27d2889426ecdf79641ce8460fc4cd25
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-17 15:47:55 +0100

    Defined Future's nothing else.

---
+++ lib/services/firebase_db.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseDb {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  Future<void> addPublicDeck() async {}
+  Future<void> deletePublicDeck() async {}
+}
