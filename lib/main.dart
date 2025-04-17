08ce415a5a7b508bd45efeeecfae99af11139f12
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-17 14:34:17 +0100

    Initialised Firebase when loading the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flashcard_app/viewmodels/auth_viewmodel.dart';
 import 'package:flashcard_app/viewmodels/deck_viewmodel.dart';
 import 'package:flashcard_app/viewmodels/edit_deck_viewmodel.dart';
 import 'package:flashcard_app/viewmodels/flashcard_viewmodel.dart';
 import 'package:flashcard_app/viewmodels/new_deck_viewmodel.dart';
 import 'package:flashcard_app/views/deck_screen.dart';
+import 'package:flashcard_app/views/landing_screen.dart';
 import 'package:flashcard_app/widgets/themes/main_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 // import 'package:flashcard_app/services/flashcard_db.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyFlashCardApp());
   // await FlashcardDb.deleteDatabaseFile();
-  // await FlashcardDb.getAllCards();
-  // await FlashcardDb.getAllDecks();
 }
 
 class MyFlashCardApp extends StatelessWidget {
@@ -22,6 +25,7 @@ class MyFlashCardApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (context) => AuthViewModel()),
         ChangeNotifierProvider(create: (context) => NewDeckViewmodel()),
         ChangeNotifierProvider(create: (context) => FlashcardViewModel()),
         ChangeNotifierProvider(create: (context) => DeckViewModel()),
@@ -29,7 +33,7 @@ class MyFlashCardApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: DeckScreen(),
+        home: LandingScreen(),
         theme: globalTheme,
       ),
     );
