6a2f2448e66eb0d9c8eb7862c9738e6adee722bb
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-17 14:34:47 +0100

    Basic functions for authenticating email and password.

---
+++ lib/viewmodels/auth_viewmodel.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthViewModel extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+
+  User? get user => _user;
+  bool get isLoggedIn => _user != null;
+
+  AuthViewModel() {
+    _auth.authStateChanges().listen((User? user) {
+      _user = user;
+      notifyListeners();
+    });
+  }
+
+  Future<void> register(String email, String password, String username) async {
+    try {
+      final newUser = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final user = newUser.user!.uid;
+
+      await FirebaseFirestore.instance.collection('users').doc(user).set({
+        'username': username,
+        'email': email,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
