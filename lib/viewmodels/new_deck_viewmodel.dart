13875dd56c5793c4259155d82a35662735783977
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-18 16:14:21 +0100

    Function to send data to Firebase service. Called in the addDeck func if isPublic

--- lib/viewmodels/new_deck_viewmodel.dart
+++ lib/viewmodels/new_deck_viewmodel.dart
@@ -1,5 +1,7 @@
 import 'package:flashcard_app/models/deck_model.dart';
+import 'package:flashcard_app/models/firebase_deck_model.dart';
 import 'package:flashcard_app/models/flashcard_model.dart';
+import 'package:flashcard_app/services/firebase_db.dart';
 import 'package:flashcard_app/services/flashcard_db.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
@@ -49,15 +51,26 @@ class NewDeckViewmodel extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> addNewDeck() async {
+  Future<void> addNewDeck(String id) async {
     DeckModel newDeck = DeckModel(
         id: theDeckId,
         title: deckTitle,
         isPublic: isPublic,
         cardCount: _newFlashcards.length);
 
+    if (newDeck.isPublic) {
+      setPublicDeck(newDeck, id);
+    }
+
     await FlashcardDb.addDeck(newDeck);
     await FlashcardDb.addDeckFlashcards(_newFlashcards);
+    notifyListeners();
+  }
+
+  Future<void> setPublicDeck(DeckModel deck, String id) async {
+    FirebaseDeckModel newDeck = FirebaseDeckModel.fromLocal(deck, id);
+
+    await FirebaseDb.addPublicDeck(newDeck, id, _newFlashcards);
     notifyListeners();
   }
 
