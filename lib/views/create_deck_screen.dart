9747a6892d1ef0a2772aaed6f922b24c00a99d6b
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-22 15:54:49 +0100

    Cleaned up code. Has the image upload

--- lib/views/create_deck_screen.dart
+++ lib/views/create_deck_screen.dart
@@ -2,6 +2,7 @@ import 'package:flashcard_app/models/flashcard_model.dart';
 import 'package:flashcard_app/viewmodels/auth_viewmodel.dart';
 import 'package:flashcard_app/viewmodels/new_deck_viewmodel.dart';
 import 'package:flashcard_app/widgets/appbar_widget.dart';
+import 'package:flashcard_app/widgets/themes/main_themes.dart';
 import 'package:flutter/material.dart';
 // import 'package:path/path.dart';
 import 'package:provider/provider.dart';
@@ -71,134 +72,146 @@ class _CreateDeckScreenState extends State<CreateDeckScreen> {
   @override
   Widget build(BuildContext context) {
     final userID = Provider.of<AuthViewModel>(context).userId;
+    final viewModel = Provider.of<NewDeckViewmodel>(context, listen: false);
 
-    return Consumer<NewDeckViewmodel>(
-      builder: (context, viewModel, child) {
-        return SafeArea(
-            child: Scaffold(
-          appBar: AppbarWidget(title: "New Deck"),
-          body: Column(
-            children: [
-              Padding(
+    return Scaffold(
+      appBar: AppbarWidget(title: "New Deck"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
                 padding: const EdgeInsets.all(7.5),
-                child: TextField(
-                  controller: _titleController,
-                  decoration: InputDecoration(
-                    labelText: "Deck Title",
-                  ),
-                  onChanged: viewModel.setDeckTitle,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(7.5),
-                child: SwitchListTile(
-                    title: Text(viewModel.publicOrPrivateLabel),
-                    value: viewModel.isPublic,
-                    onChanged: viewModel.setIsPublic),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(7.5),
-                child: TextField(
-                  controller: _cardFrontController,
-                  decoration: InputDecoration(
-                    labelText: "Card Front",
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(7.5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    IconButton(
-                        color: Color(0xFFEEA83B),
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(labelText: "Deck Title"),
+                      onChanged: viewModel.setDeckTitle,
+                    ),
+                    SizedBox(height: 10),
+                    SwitchListTile(
+                      title: Text(viewModel.publicOrPrivateLabel),
+                      value: viewModel.isPublic,
+                      onChanged: viewModel.setIsPublic,
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: _cardFrontController,
+                      decoration: InputDecoration(labelText: "Card Front"),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      decoration: addImgContainer,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                              color: Color(0xFFEEA83B),
+                              onPressed: () {
+                                viewModel.captureImg(isFront: true);
+                              },
+                              icon: Icon(Icons.camera_alt)),
+                          IconButton(
+                              color: Color(0xFFEEA83B),
+                              onPressed: () {
+                                viewModel.galleryImg(isFront: true);
+                              },
+                              icon: Icon(Icons.image)),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: _cardBackController,
+                      decoration: InputDecoration(labelText: "Card Back"),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      decoration: addImgContainer,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                              color: Color(0xFFEEA83B),
+                              onPressed: () {
+                                viewModel.captureImg(isFront: false);
+                              },
+                              icon: Icon(Icons.camera_alt)),
+                          IconButton(
+                              color: Color(0xFFEEA83B),
+                              onPressed: () {
+                                viewModel.galleryImg(isFront: false);
+                              },
+                              icon: Icon(Icons.image)),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
                         onPressed: () {
-                          viewModel.captureImg(isFront: true);
+                          viewModel.addFlashcard(_cardFrontController.text,
+                              _cardBackController.text);
+
+                          _cardFrontController.clear();
+                          _cardBackController.clear();
                         },
-                        icon: Icon(Icons.camera_alt)),
-                    IconButton(
-                        color: Color(0xFFEEA83B),
-                        onPressed: () {
-                          viewModel.galleryImg(isFront: true);
-                        },
-                        icon: Icon(Icons.image)),
+                        child: Text("Add Flashcard"),
+                      ),
+                    ),
+                    Consumer<NewDeckViewmodel>(
+                        builder: (context, viewModel, child) {
+                      return Column(
+                        children: [
+                          ...viewModel.flashcards.map((item) {
+                            int index = viewModel.flashcards.indexOf(item);
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 5.0),
+                              child: ListTile(
+                                title: Text(item.cardFront ?? ''),
+                                subtitle: Text(item.cardBack ?? ''),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                        icon: Icon(Icons.edit),
+                                        onPressed: () => _showEditBox(
+                                            context, index, item, viewModel)),
+                                    IconButton(
+                                        icon: Icon(Icons.delete),
+                                        onPressed: () =>
+                                            viewModel.removeFlashcard(index)),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                        ],
+                      );
+                    }),
+                    SizedBox(height: 80),
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(7.5),
-                child: TextField(
-                  controller: _cardBackController,
-                  decoration: InputDecoration(
-                    labelText: "Card Back",
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(7.5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                        color: Color(0xFFEEA83B),
-                        onPressed: () {
-                          viewModel.captureImg(isFront: false);
-                        },
-                        icon: Icon(Icons.camera_alt)),
-                    IconButton(
-                        color: Color(0xFFEEA83B),
-                        onPressed: () {
-                          viewModel.galleryImg(isFront: false);
-                        },
-                        icon: Icon(Icons.image)),
-                  ],
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    viewModel.addFlashcard(
-                        _cardFrontController.text, _cardBackController.text);
-
-                    _cardFrontController.clear();
-                    _cardBackController.clear();
-                  },
-                  child: Text("Add Flashcard")),
-              Expanded(
-                  child: ListView.builder(
-                      itemCount: viewModel.flashcards.length,
-                      itemBuilder: (context, index) {
-                        final item = viewModel.flashcards[index];
-                        return Padding(
-                          padding: const EdgeInsets.all(7.5),
-                          child: ListTile(
-                            title: Text(item.cardFront!),
-                            subtitle: Text(item.cardBack!),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                    icon: Icon(Icons.edit),
-                                    onPressed: () => _showEditBox(
-                                        context, index, item, viewModel)),
-                                IconButton(
-                                    icon: Icon(Icons.delete),
-                                    onPressed: () =>
-                                        viewModel.removeFlashcard(index)),
-                              ],
-                            ),
-                          ),
-                        );
-                      })),
-              ElevatedButton(
-                  onPressed: () {
-                    viewModel.addNewDeck(userID!);
-                    Navigator.pop(context);
-                  },
-                  child: Text("Save Deck")),
-            ],
+            ),
           ),
-        ));
-      },
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: ElevatedButton(
+              onPressed: () {
+                viewModel.addNewDeck(userID!);
+                Navigator.pop(context);
+              },
+              child: Text("Save Deck"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
