97a5579970dc475cfaaddcbb94ec94021d88b383
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-25 17:13:26 +0100

    Started the usage wrapper

---
+++ lib/usage_tracker.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppUsageTracker extends StatefulWidget {
+  const AppUsageTracker({super.key});
+
+  @override
+  State<AppUsageTracker> createState() => _AppUsageTracker();
+}
+
+class _AppUsageTracker extends State<AppUsageTracker>
+    with WidgetsBindingObserver {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
