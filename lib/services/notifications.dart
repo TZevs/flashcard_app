245171b5c4dcbe719f017b423b953f028b18617b
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-25 13:39:04 +0100

    Reminder notification.

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -12,7 +12,14 @@ class Notifications {
           channelKey: "end_of_deck",
           channelName: "End of Deck",
           channelDescription: "Notification for reaching the end of a deck",
+          playSound: true,
         ),
+        NotificationChannel(
+          channelKey: "daily_reminder",
+          channelName: "Daily Reminder",
+          channelDescription: "Daily reminder to review decks.",
+          enableVibration: true,
+        )
       ],
       debug: true,
     );
@@ -23,8 +30,8 @@ class Notifications {
   }
 
   static Future<void> displayEndOfDeckNotification({
-    required final String notificationTitle,
-    required final String notificationBody,
+    required String notificationTitle,
+    required String notificationBody,
   }) async {
     await AwesomeNotifications().createNotification(
       content: NotificationContent(
@@ -38,11 +45,38 @@ class Notifications {
     );
   }
 
+  static Future<void> displayReminderNotification() async {
+    await AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 2,
+        channelKey: "daily_reminder",
+        title: "Daily Reminder!",
+        body: "Don't forget to review your decks today",
+        notificationLayout: NotificationLayout.Default,
+      ),
+      schedule: NotificationCalendar(
+        hour: 17,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+        repeats: true,
+        timeZone: await AwesomeNotifications.localTimeZoneIdentifier,
+      ),
+    );
+  }
+
   static Future<void> onActionReceivedMethod(
     ReceivedAction action,
   ) async {
-    if (action.payload?['navigate'] == 'decks_screen') {
-      navigatorKey.currentState?.pushNamed('/decks_screen');
+    final String? channelKey = action.channelKey;
+
+    switch (channelKey) {
+      case "end_of_deck":
+        if (action.payload?['navigate'] == 'decks_screen') {
+          navigatorKey.currentState?.pushNamed('/decks_screen');
+        }
+      default:
+        break;
     }
   }
 }
