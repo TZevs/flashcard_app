67015081e0e54135e05894af3476b5af3b4fb1ed
Thalia <148969711+TZevs@users.noreply.github.com>
2025-04-29 17:07:17 +0100

    fake firestore package and test.

--- test/firebase_deck_model_test.dart
+++ test/firebase_deck_model_test.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flashcard_app/models/deck_model.dart';
 import 'package:flashcard_app/models/firebase_deck_model.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -45,7 +46,7 @@ void main() {
     expect(result['tags'], expected['tags']);
   });
 
-  test('Convert a Local Deck Model to a Firebase Deck Model', () {
+  test('Convert a Local DeckModel to a FirebaseDeckModel', () {
     // Arrange
     final model = DeckModel(
         id: "123", title: "TestingModel", isPublic: true, cardCount: 8);
@@ -64,6 +65,55 @@ void main() {
 
     // Act
     final result = FirebaseDeckModel.fromLocal(model, "456", "Test789");
+
+    // Assert
+    expect(result.id, expected.id);
+    expect(result.title, expected.title);
+    expect(result.userId, expected.userId);
+    expect(result.username, expected.username);
+    expect(result.createdAt.seconds, expected.createdAt.seconds);
+    expect(result.isPublic, expected.isPublic);
+    expect(result.cardCount, expected.cardCount);
+    expect(result.savedCount, expected.savedCount);
+    expect(result.tags, expected.tags);
+  });
+
+  test('Convert a Firebase QueryDocumentSnapshot to a FirebaseDeckModel',
+      () async {
+    // Arrange
+    final FakeFirebaseFirestore fakeFirebaseFirestore = FakeFirebaseFirestore();
+    final Map<String, dynamic> fakeDocData = {
+      'id': "123",
+      'title': "TestingQueryDoc",
+      'userId': "456",
+      'username': "Test789",
+      'createdAt': Timestamp.now(),
+      'isPublic': true,
+      'cardCount': 11,
+      'savedCount': 3,
+      'tags': [],
+    };
+
+    await fakeFirebaseFirestore.collection("decks").add(fakeDocData);
+
+    final documentSnapshot =
+        await fakeFirebaseFirestore.collection("decks").get();
+    final queryDocSnapshot = documentSnapshot.docs.first;
+
+    final expected = FirebaseDeckModel(
+      id: "123",
+      title: "TestingQueryDoc",
+      userId: "456",
+      username: "Test789",
+      createdAt: fakeDocData['createdAt'],
+      isPublic: true,
+      cardCount: 11,
+      savedCount: 3,
+      tags: [],
+    );
+
+    // Act
+    final result = FirebaseDeckModel.fromFirestore(queryDocSnapshot);
 
     // Assert
     expect(result.id, expected.id);
